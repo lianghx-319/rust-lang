926b1e8e6895867de35fc6a31e4114289c21e455
Hanxx <xsytby1112@gmail.com>
2021-08-01 19:38:33 +0800

    feat: loop guess a number

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,27 @@ fn main() {
     println!("Guess Number Game!");
 
     let secret_number = rand::thread_rng().gen_range(1..101);
-    println!("Secret Number is: {}", secret_number);
 
-    println!("Please guess a number: ");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Invalid line content!");
+    loop {
+        println!("Please guess a number: ");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Invalid line content!");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    println!("Number You Guess is: {}", guess);
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("To big!"),
-        Ordering::Equal => println!("Bingo!"),
+        println!("Number You Guess is: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("Bingo!");
+                break;
+            }
+        }
     }
 }
