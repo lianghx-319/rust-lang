f9f91fce056d6c796b49e4db1d71aed7f3cf9a8a
Hanxx <xsytby1112@gmail.com>
2021-08-01 19:29:55 +0800

    feat: guess a number once

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{cmp::Ordering, io};
 
 use rand::Rng;
 
@@ -6,14 +6,20 @@ fn main() {
     println!("Guess Number Game!");
 
     let secret_number = rand::thread_rng().gen_range(1..101);
-
     println!("Secret Number is: {}", secret_number);
 
     println!("Please guess a number: ");
-
     let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Invalid line content!");
 
-    io::stdin().read_line(&mut guess).expect("Invalid line content!");
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    println!("Number You Guess is: {}", guess);
 
-    println!("Number You Guess is: {}", guess.trim());
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("To big!"),
+        Ordering::Equal => println!("Bingo!"),
+    }
 }
