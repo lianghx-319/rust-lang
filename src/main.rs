8edca3a1ed85dc00d0efb3d240b65784d4f99a0f
Hanxx <xsytby1112@gmail.com>
2021-08-01 18:48:55 +0800

    feat: read number from command line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, Cargo!");
+    println!("Guess Number Game!");
+
+    println!("Please guess a number: ");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Invalid line content!");
+
+    println!("Number: {} is what you guessed", guess.trim());
 }
