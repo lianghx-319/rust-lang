277e6e7b24091f3b4a16173ce585bf11b2effe15
Hanxx <xsytby1112@gmail.com>
2021-08-04 20:52:22 +0800

    chore: 各种进制的 number

--- src/main.rs
+++ src/main.rs
@@ -16,4 +16,12 @@ fn main() {
 
     let guess: u32 = "42".parse().expect("Not a number");
     println!("{}", guess);
+
+    // number
+    let decimal = 98_222;
+    let hex = 0xff;
+    let octal = 0o77;
+    let binary = 0b1111_0000;
+    let byte = b'B';
+    println!("{}, {}, {}, {}, {}", decimal, hex, octal, binary, byte);
 }
