33faf65211b1564931b0836d6770e713bf3aafdb
Hanxx <xsytby1112@gmail.com>
2021-08-04 20:42:03 +0800

    chore: variable shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 fn main() {
-    let mut x = 5;
+    let x = 5;
     println!("The value of x is {}", x);
 
-    x = 6;
+    let x = x + 1;
+    let x = x * 2;
     println!("The value of x is {}", x);
 
     const MAX_POINTS: u32 = 100_000;
