c592b08cbe367dd1ee61d872ed9d868f53266364
Hanxx <xsytby1112@gmail.com>
2021-08-25 22:30:27 +0800

    string slice three

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 fn main() {
-    let s = String::from("Hello world");
+    let first = first_world("hello world");
+    println!("{}", first);
+}
 
-    let hello = &s[..5];
-    let world = &s[6..];
-    let whole = &s[..];
-
-    println!("{}, {}, {}", hello, world, whole);
+fn first_world(s: &str) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+    return &s[..];
 }
