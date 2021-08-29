ffc29b8c23b0b670372e115e50c7e00a4d2adb60
Hanxx <xsytby1112@gmail.com>
2021-08-29 10:43:51 +0800

    struct 关联函数

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,17 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.length
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.length > other.length
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            length: size,
+        }
+    }
 }
 
 fn main() {
@@ -15,7 +26,18 @@ fn main() {
         width: 30,
         length: 50,
     };
-    println!("{}", rect.area());
+    let rect1 = Rectangle {
+        width: 10,
+        length: 40,
+    };
+    let rect2 = Rectangle {
+        width: 35,
+        length: 55,
+    };
+    let s = Rectangle::square(20);
+    println!("{}", s.area());
 
-    println!("{:#?}", rect);
+    println!("{}", rect1.can_hold(&rect2));
+
+    println!("{}", rect2.can_hold(&rect));
 }
