eface3928902d194f907e391edf1f67759e46f26
Hanxx <xsytby1112@gmail.com>
2021-08-29 10:35:59 +0800

    struct method use impl

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,18 @@ struct Rectangle {
     length: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.length
+    }
+}
+
 fn main() {
     let rect = Rectangle {
         width: 30,
         length: 50,
     };
-    println!("{}", area(&rect));
+    println!("{}", rect.area());
 
     println!("{:#?}", rect);
-}
-
-fn area(rect: &Rectangle) -> u32 {
-    rect.width * rect.length
 }
