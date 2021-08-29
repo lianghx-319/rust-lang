b2d4188bc5cf049d91e8271d76dca26cecc346f3
Hanxx <xsytby1112@gmail.com>
2021-08-29 10:15:30 +0800

    struct base

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
-fn main() {
-    let first = first_world("hello world");
-    println!("{}", first);
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
 }
 
-fn first_world(s: &str) -> &str {
-    let bytes = s.as_bytes();
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[..i];
-        }
+fn main() {
+    let user = build_user(String::from("a@b.com"), String::from("Han"));
+
+    println!("user: {}", user.email);
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        username,
+        email,
+        sign_in_count: 0,
+        active: true,
     }
-    return &s[..];
 }
