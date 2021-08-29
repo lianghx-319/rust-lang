27e8e1799d46d44069174359b47e20396170f872
Hanxx <xsytby1112@gmail.com>
2021-08-29 11:10:25 +0800

    null in rust is option<T>

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,9 @@
-enum IpAddrKind {
-    V4(u8, u8, u8, u8),
-    V6(String),
-}
-
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
-}
-
 fn main() {
-    let home = IpAddr {
-        kind: IpAddrKind::V4(127, 0, 0, 1),
-        address: String::from("127.0.0.1"),
-    };
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6(String::from("::1")),
-        address: String::from("::1"),
-    };
+    let x: i8 = 5;
+    let y: Option<i8> = None;
+
+    match y {
+        None => println!("y is not a number"),
+        Some(value) => println!("{}", x + value),
+    }
 }
