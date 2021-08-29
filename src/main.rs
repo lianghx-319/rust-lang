658c5c1eff373be85bb90728de7571993bbbe73a
Hanxx <xsytby1112@gmail.com>
2021-08-29 10:58:14 +0800

    enum

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,20 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    length: u32,
+enum IpAddrKind {
+    V4(u8, u8, u8, u8),
+    V6(String),
 }
 
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.length
-    }
-
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.length > other.length
-    }
-
-    fn square(size: u32) -> Rectangle {
-        Rectangle {
-            width: size,
-            length: size,
-        }
-    }
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
 }
 
 fn main() {
-    let rect = Rectangle {
-        width: 30,
-        length: 50,
+    let home = IpAddr {
+        kind: IpAddrKind::V4(127, 0, 0, 1),
+        address: String::from("127.0.0.1"),
     };
-    let rect1 = Rectangle {
-        width: 10,
-        length: 40,
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6(String::from("::1")),
+        address: String::from("::1"),
     };
-    let rect2 = Rectangle {
-        width: 35,
-        length: 55,
-    };
-    let s = Rectangle::square(20);
-    println!("{}", s.area());
-
-    println!("{}", rect1.can_hold(&rect2));
-
-    println!("{}", rect2.can_hold(&rect));
 }
